c03ba6325456657b5ee2a3cff8279f822bbb17e8
ChiragChrg <chiruchirag2001@gmail.com>
2024-06-30 18:15:45 +0530

    Initialized Next-PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextPWA from 'next-pwa';
+
+const withPWA = nextPWA({
+    dest: 'public',
+    register: true,
+    disable: process.env.NODE_ENV === "development"
+})
+
+const nextConfig = withPWA({
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: "lh3.googleusercontent.com",
+            },
+            {
+                protocol: 'https',
+                hostname: "avatars.githubusercontent.com",
+            },
+            {
+                protocol: 'https',
+                hostname: "gravatar.com",
+            },
+        ],
+    }
+})
 
 export default nextConfig;
