10397f9cc5142103b0a6bee66d658b46f917d7c6
ChiragChrg <chiruchirag2001@gmail.com>
2024-06-30 23:18:33 +0530

    Initialized PWA

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='w-full flex justify-between items-center px-4 py-4 bg-gray-300'>
+            <h1>AIMS</h1>
+
+            <nav className="flex justify-between items-center gap-4">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+                <a href="#">AIMIT</a>
+                <a href="#">Get Started</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
