5038cd7dcf4726c0bdcf3c3d9c61c67389debf41
ChiragChrg <chiruchirag2001@gmail.com>
2024-07-14 22:25:04 +0530

    Added GradMaster Logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
+import LogoSVG from './LogoSVG'
 
 const Header = () => {
     return (
         <div className='w-full flex justify-between items-center px-4 py-4 bg-gray-300'>
-            <h1>GradMaster</h1>
+            <LogoSVG size={80} />
 
             <nav className="flex justify-between items-center gap-4">
                 <a href="#">Home</a>
