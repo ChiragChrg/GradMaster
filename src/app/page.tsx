ad17cb8fb3ec2b0cc3f673cdf247d948cef0223c
ChiragChrg <chiruchirag2001@gmail.com>
2024-07-21 18:30:22 +0530

    Initial Landing page UI Done ✨

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,10 +15,17 @@ export default function Home() {
     <HeroHighlight>
       <Header />
 
-      <div className=" relative flex_center mt-[10em]">
+      <div className=" relative flex_center mt-[8em]">
         <LogoSVG color="hsl(var(--primary))" className="z-10" />
-        <span className="absolute bg-primary/30 w-[500px] aspect-square rounded-full opacity-0 animate-ripple"></span>
-        <span className="absolute bg-primary/30 w-[500px] aspect-square rounded-full opacity-0 animate-ripple delay-1000"></span>
+        <span className="absolute bg-background w-[100px] aspect-square rounded-full z-[5]"></span>
+        <span className="absolute bg-radialGradient w-[400px] md:w-[500px] aspect-square rounded-full opacity-0 animate-ripple"></span>
+        <span className="absolute bg-radialGradient w-[400px] md:w-[500px] aspect-square rounded-full opacity-0 animate-ripple delay-1000"></span>
+      </div>
+
+      <div className="flex_center flex-col mt-12 p-4">
+        <h1 className="text-[2.5em] font-bold tracking-wider mt-4">GradMaster</h1>
+        {/* <h2 className="text-[1.2em]">Graduate Mastery at your fingertips</h2> */}
+        <h2 className="text-[1.2em] text-center">Transforming Academic Management with Innovation and Insight</h2>
       </div>
     </HeroHighlight>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,9 +11,6 @@ const Header = () => {
             </a>
 
             <nav className="flex justify-between items-center gap-4">
-                <a href="#">About</a>
-                <a href="#">Contact</a>
-
                 <ThemeButton />
                 <a className='bg-primary px-4 py-2 rounded-md text-white' href="#">Get Started</a>
             </nav>
